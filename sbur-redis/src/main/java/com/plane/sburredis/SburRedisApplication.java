94e15a28eea3644101154626591444a9f6067f8b
Juice <3221ju@gmail.com>
2024-01-21 20:31:16 +0900

    feat: PlaneFinderPoller 추가

--- sbur-redis/src/main/java/com/plane/sburredis/SburRedisApplication.java
+++ sbur-redis/src/main/java/com/plane/sburredis/SburRedisApplication.java
@@ -2,6 +2,13 @@ package com.plane.sburredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @SpringBootApplication
 public class SburRedisApplication {
@@ -10,4 +17,16 @@ public class SburRedisApplication {
 		SpringApplication.run(SburRedisApplication.class, args);
 	}
 
+	@Bean
+	public RedisOperations<String, Aircraft> redisOperations(RedisConnectionFactory factory) {
+		Jackson2JsonRedisSerializer<Aircraft> serializer = new Jackson2JsonRedisSerializer<>(
+			Aircraft.class);
+
+		RedisTemplate<String, Aircraft> template = new RedisTemplate<>();
+		template.setConnectionFactory(factory);
+		template.setDefaultSerializer(serializer);
+		template.setKeySerializer(new StringRedisSerializer());
+
+		return template;
+	}
 }
