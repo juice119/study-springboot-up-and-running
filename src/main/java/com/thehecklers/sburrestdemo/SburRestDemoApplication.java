d7ddaae3c4d13bf78896ee8e279f0dcaefd68af0
Juice <3221ju@gmail.com>
2024-01-21 17:33:56 +0900

    feat: @ConfigurationProperty 로 PropertySource 사용

--- src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
@@ -4,7 +4,13 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.thehecklers.sburrestdemo.coffee.Coffee;
 import com.thehecklers.sburrestdemo.coffee.CoffeeRepository;
@@ -15,12 +21,54 @@ import jakarta.annotation.PostConstruct;
  * 처음부터 배우는 자바 예제
  */
 @SpringBootApplication
+@ConfigurationPropertiesScan // @ConfigurationProperty 클래스 스캔
 public class SburRestDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SburRestDemoApplication.class, args);
 	}
 
+	@Bean
+	@ConfigurationProperties(prefix = "droid")
+	Droid createDroid() {
+		return new Droid();
+	}
+
+}
+
+@RestController
+@RequestMapping("/droid")
+class DroidController {
+	private final Droid droid;
+
+	public DroidController(Droid droid) {
+		this.droid = droid;
+	}
+
+	@GetMapping
+	Droid getDroid() {
+		return droid;
+	}
+}
+
+class Droid {
+	private String id, description;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
 
 @Component
--- src/main/java/com/thehecklers/sburrestdemo/greeting/GreetingController.java
+++ src/main/java/com/thehecklers/sburrestdemo/greeting/GreetingController.java
@@ -1,26 +1,28 @@
 package com.thehecklers.sburrestdemo.greeting;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.thehecklers.sburrestdemo.config.Greeting;
+
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-	@Value("${greeting-name: Mirage}")
-	private String name;
 
-	@Value("${greeting-coffee: ${greeting-name} is drinking cafe Ganador}")
-	private String coffee;
+	private final Greeting greeting;
+
+	public GreetingController(Greeting greeting) {
+		this.greeting = greeting;
+	}
 
 	@GetMapping
 	String getGreeting() {
-		return name;
+		return greeting.getName();
 	}
 
 	@GetMapping("/coffee")
 	String getCoffee() {
-		return coffee;
+		return greeting.getCoffee();
 	}
 }
