95c743845cd3552c7110f566ad50fa714794d52f
Juice <3221ju@gmail.com>
2024-01-21 12:38:13 +0900

    feat: @Value 로 PropertySource 사용하기

---
+++ src/main/java/com/thehecklers/sburrestdemo/greeting/GreetingController.java
@@ -0,0 +1,26 @@
+package com.thehecklers.sburrestdemo.greeting;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+	@Value("${greeting-name: Mirage}")
+	private String name;
+
+	@Value("${greeting-coffee: ${greeting-name} is drinking cafe Ganador}")
+	private String coffee;
+
+	@GetMapping
+	String getGreeting() {
+		return name;
+	}
+
+	@GetMapping("/coffee")
+	String getCoffee() {
+		return coffee;
+	}
+}
