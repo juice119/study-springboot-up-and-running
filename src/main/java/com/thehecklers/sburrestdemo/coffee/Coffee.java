06c3202c2ecefba5e45423f9b97f7f4bc2dfc87d
Juice <3221ju@gmail.com>
2024-01-21 11:41:28 +0900

    ref: 프로젝트 구조 도메인 기준으로 변경

---
+++ src/main/java/com/thehecklers/sburrestdemo/coffee/Coffee.java
@@ -0,0 +1,34 @@
+package com.thehecklers.sburrestdemo.coffee;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+class Coffee {
+	@Id
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
